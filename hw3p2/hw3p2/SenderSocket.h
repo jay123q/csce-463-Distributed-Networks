6c6010206fdbc27e0d8df7d15e1f7f3bb8ef9a5f
jay123q <106704608+jay123q@users.noreply.github.com>
2023-10-24 13:47:20 -0500

    start hw3 p2

---
+++ hw3p2/hw3p2/SenderSocket.h
@@ -0,0 +1,78 @@
+#pragma once
+#include "pch.h"
+#include <ctype.h> 
+#include <stdio.h> 
+#include <windows.h>
+#include <string>
+#include <iostream>
+using namespace std;
+
+
+#pragma pack(push,1) // restores old packing
+class LinkProperties {
+public:
+    // transfer parameters
+    float RTT; // propagation RTT (in sec)
+    float speed; // bottleneck bandwidth (in bits/sec)
+    float pLoss[2]; // probability of loss in each direction
+    DWORD bufferSize; // buffer size of emulated routers (in packets)
+    LinkProperties() { memset(this, 0, sizeof(*this)); }
+};
+
+#define MAGIC_PROTOCOL 0x8311AA
+class Flags {
+public:
+    DWORD reserved : 5; // must be zero
+    DWORD SYN : 1;
+    DWORD ACK : 1;
+    DWORD FIN : 1;
+    DWORD magic : 24;
+    Flags() { memset(this, 0, sizeof(*this)); magic = MAGIC_PROTOCOL; }
+};
+
+class SenderDataHeader {
+public:
+    Flags flags;
+    DWORD seq; // must begin from 0
+};
+class ReceiverHeader {
+public:
+    Flags flags;
+    DWORD recvWnd; // receiver window for flow control (in pkts)
+    DWORD ackSeq; // ack value = next expected sequence
+};
+class SenderSynHeader {
+public:
+    SenderDataHeader sdh;
+    LinkProperties lp;
+};
+
+#pragma pack(pop,1) // restores old packing
+
+
+class SenderSocket {
+    struct sockaddr_in local;
+    struct hostent* remote;
+    struct sockaddr_in server;
+    int bytes;
+    LinkProperties* lp;
+    string host;
+    DWORD IP;
+
+public:
+    int bytesRec;
+    bool opened;
+    double RTT;
+    SOCKET sock;
+    clock_t time;
+    double closeCalledTime;
+    clock_t startRTT;
+    SenderSynHeader * packetSyn;
+    SenderSynHeader * packetFin;
+    SenderSocket();
+    DWORD Open(string host, int portNumber, int senderWindow, LinkProperties* lp);
+    DWORD Send(char* pointer, UINT64 bytes );
+    DWORD recvFrom(long RTOsec, long RTOusec, bool inOpen);
+    DWORD Close();
+
+};
--- hw3p2/hw3p2/hw3p2.cpp
+++ hw3p2/hw3p2/hw3p2.cpp
@@ -21,17 +21,63 @@
 #define MAGIC_PORT 22345 // receiver listens on this port
 #define MAX_PKT_SIZE (1500-28) // maximum UDP packet size accepted by receiver 
 
-#pragma comment(lib, "ws2_32")
 
 #include "pch.h"
 
 using namespace std;
+struct statsThread {
+    HANDLE statsEvent;
+    clock_t startTimer;
+    int nummberThread;
+    int qSize;
 
+};
+DWORD WINAPI status_thread(LPVOID that)
+{
+
+    bool printMe = false;
+    while ((WaitForSingleObject(this->statusEvent, 2000) == WAIT_TIMEOUT) || !printMe)
+    {
+
+        printMe = true;
+        printf("[%3d] %4d Q %6d E %7d H %6d D  %6d I %5d R %5d C %5d L %4dK\n",
+            (int)(double)(clock() - this->startTimer) / CLOCKS_PER_SEC,
+            this->numberThread,
+            (int)this->q.size(),
+            this->parserStats->numberExtractedURL,
+            this->parserStats->numberUniqueHost,
+            this->parserStats->numberDnsLookup,
+            this->parserStats->numberIpUnique,
+            this->parserStats->numberRobotPass,
+            this->parserStats->numberSuccessfullyCrawled, // this is non robbot pages
+            this->parserStats->numberTotalLinks / 1000);
+
+        //	this->bytesDownloadedInBatch = (this->parserStats->newNumberBytesInBatch - this->totalBytes) / (double)((clock() - this->startTimer) / CLOCKS_PER_SEC);
+        //	this->pagesDownloadedInBatch = (this->parserStats->newNumberPagesInBatch - this->totalPages) / (double)((clock() - this->startTimer) / CLOCKS_PER_SEC);	
+        // 
+        // here	
+        this->bytesDownloadedInBatch = (8 * (this->parserStats->newNumberBytesInBatch - this->totalBytes) / (double)((clock() - this->startTimer) / CLOCKS_PER_SEC));
+        this->pagesDownloadedInBatch = (this->parserStats->newNumberPagesInBatch - this->totalPages) / (double)((clock() - this->startTimer) / CLOCKS_PER_SEC);
+
+        // Output crawling information
+        printf("\t*** crawling %.1f pps @ %.1f Mbps\n", this->pagesDownloadedInBatch, this->bytesDownloadedInBatch / double(125000));
+        this->totalBytes = this->parserStats->newNumberBytesInBatch;
+        this->totalPages = this->parserStats->newNumberPagesInBatch;
+        // this->parserStats->newNumberBytesInBatch = 0;
+        // this->parserStats->newNumberPagesInBatch = 0;
+
+        // pass the status on to the next person
+
+        ResetEvent(statusEvent);
+    }
+    return 0;
+
+}
 
 
 void main(int argc, char** argv)
 {
-     LinkProperties linkedProperties;
+    LinkProperties linkedProperties;
 
 
 
@@ -42,7 +88,7 @@ void main(int argc, char** argv)
    // std::string host("128.194.135.82");
    // std::string host("0.0.0.0");
    // std::string host("127.0.0.1");
-    
+
     int power = 24;
     int sendingWindow = 50000;
     linkedProperties.RTT = 0.2;
@@ -59,12 +105,13 @@ void main(int argc, char** argv)
     std::string host(argv[1]);
     int power = atoi(argv[2]);
     int sendingWindow = atoi(argv[3]);
-    linkedProperties.RTT = atof( argv[4] );
-    linkedProperties.pLoss[FORWARD_PATH] = atof( argv[5] );
-    linkedProperties.pLoss[RETURN_PATH] = atof( argv[6] );
-    linkedProperties.speed = atof( argv[7] );
+    linkedProperties.RTT = atof(argv[4]);
+    linkedProperties.pLoss[FORWARD_PATH] = atof(argv[5]);
+    linkedProperties.pLoss[RETURN_PATH] = atof(argv[6]);
+    linkedProperties.speed = atof(argv[7]);
 
-
+    statsThread st;
+    st.statusEvent = CreateEvent(NULL, true, false, NULL);
     clock_t timeOpen = clock();
     printf("Main:   sender W = %d, RTT %.3f sec, loss %g / %g, link %g Mbps\n",
         sendingWindow,
@@ -88,15 +135,15 @@ void main(int argc, char** argv)
     printf("Main:   initializing DWORD array with 2^%d elements... done in %d ms\n",
         power,
         ((clock() - timeOpen) * 1000) / CLOCKS_PER_SEC
-        
-        );
+
+    );
 
     if ((status = ss.Open(targetHost, MAGIC_PORT, sendingWindow, &linkedProperties)) != STATUS_OK)
     {
         // error handling: print status and quit
         printf("Main:   connect failed with status %d\n", status);
         ss.opened = false;
-                return;
+        return;
 
     }
     clock_t OpenReturnTime = clock();
@@ -128,7 +175,7 @@ void main(int argc, char** argv)
     */
     char* charBuf = (char*)dwordBuf; // this buffer goes into socket
     UINT64 byteBufferSize = dwordBufSize << 2; // convert to bytes
-        UINT64 off = 0; // current position in buffer
+    UINT64 off = 0; // current position in buffer
     UINT64 bytes = 0;
     bytes = min(byteBufferSize - off, MAX_PKT_SIZE - sizeof(SenderDataHeader));
 
@@ -136,7 +183,7 @@ void main(int argc, char** argv)
         host.c_str(),
         ss.RTT,
         bytes + ss.bytesRec
-        );
+    );
     // recieve from
     // double connectToMain = (clock() - ss.startRTT)/CLOCKS_PER_SEC;
     if ((status = ss.Close()) != STATUS_OK)
@@ -144,12 +191,12 @@ void main(int argc, char** argv)
         // error handing: print status and quit
         ss.opened = false;
         printf("Main:   connect failed with status %d\n", status);
-                return;
+        return;
 
     }
 
     printf("Main:   transfer finished in %.3f sec\n",
-          (double) ( (ss.closeCalledTime - OpenReturnTime)/CLOCKS_PER_SEC )
+        (double)((ss.closeCalledTime - OpenReturnTime) / CLOCKS_PER_SEC)
     );
 
 }
